5cbbf6d0c3b000248ec88e5030ea79ff25b0838c
Bill Majoros <martiandna@gmail.com>
2016-07-13 13:06:45 -0400

    update

--- split-train-test.pl
+++ split-train-test.pl
@@ -3,8 +3,16 @@ use strict;
 use ProgramName;
 
 my $name=ProgramName::get();
-die "$name <indir> <outdir-train> <outdir-test>\n" unless @ARGV==3;
-my ($indir,$outTrain,$outTest)=@ARGV;
+die "$name <indir> <num-test> <outdir-train> <outdir-test>\n" unless @ARGV==4;
+my ($indir,$numTest,$outTrain,$outTest)=@ARGV;
+
+my @files=`ls $indir`;
+my $n=@files;
+for(my $i=0 ; $i<$n ; ++$i) {
+  my $file=$files[$i]; chomp $file;
+  if($i<$numTest) { system("ln -s $indir/$file $outTest/$file") }
+  else { system("ln -s $indir/$file $outTrain/$file") }
+}
 
 
 
