10800886f9f1016f09f8f413813cae2416051bb6
Bill Majoros <martiandna@gmail.com>
2016-07-14 14:58:19 -0400

    update

--- graph-all.pl
+++ graph-all.pl
@@ -11,10 +11,15 @@ my ($dir)=@ARGV;
 
 my @files=`ls $dir`;
 my $n=@files;
+for(my $i=0 ; $i<$n-1 ; ++$i) {
+  my $j=$i+int(rand($n-$i));
+  my $temp=$files[$i];
+  $files[$i]=$files[$j];
+  $files[$j]=$temp;
+}
 for(my $i=0 ; $i<$n ; ++$i) {
   my $file=$files[$i]; chomp $file;
   print "$dir/$file  ---  press ENTER...\n";
   system("cat $dir/$file | $MUMMIE/fastb-to-xgraph.pl");
   <STDIN>;
-
 }
