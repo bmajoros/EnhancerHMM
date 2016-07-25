77e43c3a155eaf71556004e5dfc26c1a19d1c09a
Bill Majoros <martiandna@gmail.com>
2016-07-25 14:53:11 -0400

    update

---
+++ get-nonredundant-fastbs.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use ProgramName;
+
+my $name=ProgramName::get();
+die "$name <in-dir> <out-dir>\n" unless @ARGV==2;
+my ($indir,$outdir)=@ARGV;
+
+my %seen;
+my @files=`ls $indir`;
+my $n=@files;
+for(my $i=0 ; $i<$n ; ++$i) {
+  my $file=$files[$i]; chomp $file;
+  next unless $file=~/\.fastb$/;
+  $file=~/(\S+)\.t\d+.standardized.fastb/ || die $file;
+  my $id=$1;
+  next if $seen{$id};
+  system("cp $indir/$file $outdir");
+  $seen{$id}1=;
+}
+
+
+
+
+
