0e30a7ba4043c2c9c26c688745cb1d7a3e40f516
Bill Majoros <martiandna@gmail.com>
2016-08-04 15:26:09 -0400

    update

--- motif-scan.pl
+++ motif-scan.pl
@@ -2,7 +2,9 @@
 use strict;
 use ProgramName;
 use TempFilename;
+use Fastb;
 
+my $THRESHOLD=0.0001;
 my $MARKOV="/data/reddylab/software/meme_4.10.0/bin/fasta-get-markov";
 my $MAST="/data/reddylab/software/meme_4.10.0/bin/mast";
 my $EXTRACT="/home/bmajoros/MUMMIE/fastb-to-fasta.pl";
@@ -24,7 +26,27 @@ for(my $i=0 ; $i<$n ; ++$i) {
   system("$MARKOV -m 1 $TEMP_FASTA > $TEMP_BACKGROUND");
 
   my $MOTIF="main-motifs/GR.meme";
-  system("$MAST $MOTIF $TEMP_FASTA -hit_list -mt 0.05 -bfile $TEMP_BACKGROUND > $TEMP_OUT");
+  system("$MAST $MOTIF $TEMP_FASTA -hit_list -mt $THRESHOLD -bfile $TEMP_BACKGROUND > $TEMP_OUT");
+
+
+  open(IN,$TEMP_OUT) || die $TEMP_OUT;
+  while(<IN>) {
+    next if(/^#/);
+    chomp; my @fields=split; next unless @fields>=6;
+    my ($name,$strand,$begin,$end,$score,$P)=@fields;
+    
+  }
+  close(IN);
+
+# # All non-overlapping hits in all sequences from "tmp.fasta".
+# # sequence_name motif hit_start hit_end score hit_p-value
+# iter0_peak130864.t05.standardized -1 9 25 -1904.73 1.14e-02
+# iter0_peak130864.t05.standardized +1 33 49 -1094.17 2.93e-03
+# iter0_peak130864.t05.standardized +1 55 71 -2020.52 1.36e-02
+# iter0_peak130864.t05.standardized -1 75 91 -2252.10 1.90e-02
+# iter0_peak130864.t05.standardized +1 148 164 -1765.77 9.23e-03
+# # mast main-motifs/GR.meme tmp.fasta -hit_list -mt 0.05 -bfile tmp.bg
+
 
   exit;
 }
