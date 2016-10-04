9774cf38eb1aa5e2b438251cac69017ea5543eeb
Bill Majoros <martiandna@gmail.com>
2016-10-04 12:44:59 -0400

    subset-files.pl

--- bootstrap.pl
+++ bootstrap.pl
@@ -1,4 +1,5 @@
 #!/bin/env perl
+use strict;
 use Fastb;
 $|=1;
 
---
+++ subset-files.pl
@@ -0,0 +1,32 @@
+#!/bin/env perl
+use strict;
+
+die "subset-files.pl <file-list.txt> <num-subsets> <outdir>\n" unless @ARGV==3;
+my ($filelist,$numSets,$outDir)=@ARGV;
+
+my @files;
+open(IN,$filelist) || die $filelist;
+while(<IN>) {
+  chomp; next unless(/\.fastb$/);
+  push @files,$_;
+}
+close(IN);
+
+my $numFiles=@files;
+my $filesPerSet=$numFiles/$numSets;
+my $nextSet=1;
+my $file=">$outDir/files$nextSet.txt";
+open(OUT,$file) || die $file;
+for(my $i=0 ; $i<$numFiles ; ++$i) {
+  if($i>$nextSet*$filesPerSet) {
+    close(OUT);
+    ++$nextSet;
+    my $file=">$outDir/files$nextSet.txt";
+    open(OUT,$file) || die $file;
+  }
+  my $file=$files[$i];
+  print OUT "$file\n";
+}
+close(OUT);
+
+
