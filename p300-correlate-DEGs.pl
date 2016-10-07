15ddf4af87872b8af37eba09eed4c38815c724c2
Bill Majoros <martiandna@gmail.com>
2016-10-07 14:35:24 -0400

    update

--- p300-correlate-DEGs.pl
+++ p300-correlate-DEGs.pl
@@ -1,6 +1,7 @@
 #!/bin/env perl
 use strict;
 use SummaryStats;
+$|=1;
 
 # Globals
 my $BASE="/home/bmajoros/GGR/p300";
@@ -10,15 +11,17 @@ my $PEAKS="$BASE/peaks.bed";
 my $GENES="$BASE/genes/DEGs_upreg.FDR_0.01.TSS.protein_coding.bed";
 
 # Load peak coordinates
+#print "loading peak coordinates\n";
 my %peaks;
 open(IN,$PEAKS) || die $PEAKS;
 while(<IN>) {
   chomp; my @fields=split; next unless @fields>=4;
   my ($chr,$begin,$end,$id)=@fields;
-  $peaks{$id} = { chr=>$chr, begin=>$begin, end=>$end }
+  $peaks{$id} = { chr=>$chr, begin=>$begin, end=>$end } }
 close(IN);
 
 # Load DEGs
+#print "loading DEGs\n";
 my %DEGs;
 open(IN,$GENES) || die $GENES;
 while(<IN>) {
@@ -29,14 +32,14 @@ while(<IN>) {
 close(IN);
 
 # Sort DEGs
+#print "sorting DEGs\n";
 my @chroms=keys %DEGs;
 foreach my $chr (@chroms) {
-  @{$DEGs{$chr}}=sort {$a <=> $b} @{$DEGs{$chr}};
-}
+  @{$DEGs{$chr}}=sort {$a <=> $b} @{$DEGs{$chr}};}
 
 # Process activating/deactivating and compare mean distances to DEGs
 my ($posDist,$posSD)=analyze($ACTIVATING);
-my ($negDist,$negSD)=$negDist=analyze($DEACTIVATING);
+my ($negDist,$negSD)=analyze($DEACTIVATING);
 print "activating: $posDist +/- $posSD\n";
 print "inactivating: $negDist +/- $negSD\n";
 
@@ -47,15 +50,18 @@ print "inactivating: $negDist +/- $negSD\n";
 #======================================================================
 sub analyze {
   my ($peaksFile)=@_;
+  #print "Analyzing $peaksFile\n";
   my @distances;
-  open(IN,$peaksFile) || die $peaksfile;
+  open(IN,$peaksFile) || die $peaksFile;
   while(<IN>) {
     chomp; next unless(/\S/);
     my $record=$peaks{$_}; die $_ unless $record;
     my $chr=$record->{chr}; my $begin=$record->{begin}; my $end=$record->{end};
     my $pos=int(($begin+$end)/2);
+    next if $chr eq "chrX" || $chr eq "chrY";
     my $dist=findNearest($chr,$pos);
     if($dist>=0) { push @distances,$dist }
+    #print "$dist\n";
   }
   close(IN);
   my ($mean,$stddev,$min,$max)=SummaryStats::roundedSummaryStats(\@distances);
@@ -64,14 +70,19 @@ sub analyze {
 #======================================================================
 sub findNearest {
   my ($chr,$pos)=@_;
+  #print "find $chr $pos\n";
   my $A=$DEGs{$chr};
+  die $chr unless $A;
   my $n=@$A;
-  my $begin=0; my $end=$n;
+  #print "N=$n\n";
+  my $begin=0; my $end=$n-1;
   while($begin<$end) {
     my $mid=int(($begin+$end)/2);
     my $midPos=$A->[$mid];
-    if($pos<$midPos) { $end=$mid }
-    else { $begin=$mid }
+    #print "mid: $mid $midPos $begin\-$end\n";
+    if($pos<$midPos) { $end=$mid-1 }
+    elsif($pos>$midPos) { $begin=$mid+1 }
+    else { $begin=$end=$mid }
   }
   die unless $begin>=0 && $begin<$n;
   my $thisPos=$A->[$begin];
@@ -86,6 +97,7 @@ sub findNearest {
     my $nextDist=abs($pos-$nextPos);
     if($nextDist<$dist) { $dist=$nextDist }
   }
+  return $dist;
 }
 #======================================================================
 #======================================================================
