343bf05f26228dc810b4174784aabe782e829f41
Bill Majoros <martiandna@gmail.com>
2016-12-30 09:20:51 -0500

    p300-heatmap.py

---
+++ p300-heatmap.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#=========================================================================
+# This is OPEN SOURCE SOFTWARE governed by the Gnu General Public
+# License (GPL) version 3, as described at www.opensource.org.
+# Copyright (C)2016 William H. Majoros (martiandna@gmail.com).
+#=========================================================================
+from __future__ import (absolute_import, division, print_function, 
+   unicode_literals, generators, nested_scopes, with_statement)
+from builtins import (bytes, dict, int, list, object, range, str, ascii,
+   chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)
+# The above imports should allow this program to run in both Python 2 and
+# Python 3.  You might need to update your version of module "future".
+import sys
+import ProgramName
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if(len(sys.argv)!=2):
+    exit(ProgramName.get()+" <trajectories.tsv>\n")
+infile=sys.argv[1]
+
+df=pd.read_csv(infile,sep="\t")
+
+plt.imshow(df, cmap='hot', interpolation='nearest')
+plt.colorbar()  
+plt.savefig("heatmap.pdf")
+
+
