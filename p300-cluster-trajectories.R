d1f814156b2260d6ee42562bb4137cb05de2d183
Bill Majoros <martiandna@gmail.com>
2016-12-30 11:58:44 -0500

    update

--- p300-cluster-trajectories.R
+++ p300-cluster-trajectories.R
@@ -6,6 +6,7 @@ data <- data[2:13]
 trinary <- ifelse(data>1000,1,ifelse(data< -1000,-1,0))
 clusters <- kmeans(trinary,10,iter.max=100)
 elements <- cbind(elements,clusters$cluster)
+write.table(elements,file="cluster-membership.txt",sep="\t", col.names = F, row.names = F)
 write.table(clusters$centers,file="clusters.txt",sep="\t", col.names = F, row.names = F)
 write.table(clusters$size,file="cluster-sizes.txt",sep="\t", col.names = F, row.names = F)
 
