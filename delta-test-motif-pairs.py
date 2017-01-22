e0154f966a11cfbc82e9ad34b189fce3ab9e6182
Bill Majoros <martiandna@gmail.com>
2017-01-22 08:01:57 -0500

    update

--- delta-test-motif-pairs.py
+++ delta-test-motif-pairs.py
@@ -20,6 +20,41 @@ def fisher(a,b,c,d):
     (ret,line)=commands.getstatusoutput(cmd)
     return float(line.rstrip())
 
+def getTable(motif1,motif2,peaks):
+    yesyes=0; yesno=0; noyes=0; nono=0
+    for peak in peaks:
+        counts=getSingletonCounts(peak)
+        bothPresent=False
+        if(motif1==motif2):
+            bothPresent=counts.get(motif1,0)==2
+        else: 
+            bothPresent=counts.get(motif1,0)>0 and\
+                counts.get(motif2,0)>0
+        if(bothPresent): 
+            yesyes+=1
+            continue
+        motif1present=counts.get(motif1,0)>0
+        motif2present=counts.get(motif2,0)>0
+        if(motif1==motif2):
+            if(motif1present):
+                yesno+=1; noyes+=1
+            else: nono+=1
+        else:
+            if(motif1present): yesno+=1
+            elif(motif2present): noyes+=1
+            else: nono+=1
+    return (yesyes,yesno,noyes,nono)
+
+def incrementHash(key,hash):
+    hash[key]=hash.get(key,0)+1
+
+def getSingletonCounts(peak):
+    counts={}
+    for pair in peak:
+        incrementHash(pair[0],counts)
+        incrementHash(pair[1],counts)
+    return counts
+
 #=========================================================================
 # main()
 #=========================================================================
@@ -47,4 +82,15 @@ IN.close()
 for motif1 in motifs:
     for motif2 in motifs:
         if(motif1>motif2): continue
-        
+        (a,b,c,d)=getTable(motif1,motif2,peaks)
+        P=fisher(a,b,c,d)
+        if(P<0.05):
+            N=float(a+b+c+d)
+            Ptop=float(a+b)/N
+            Pbottom=1.0-Ptop
+            expA=int(Ptop*float(a+c))
+            #expB=Ptop*float(b+d)
+            #expC=Pbottom*float(a+c)
+            #expD=Pbottom*float(b+d)
+            if(a<=expA): continue
+            print(P,motif1,motif2,a,expA,sep="\t")
